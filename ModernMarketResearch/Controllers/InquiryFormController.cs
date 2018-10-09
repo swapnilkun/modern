c21a4511a1a98733f521798713157d07ba1baf36
Naveen16291 <balasahebpatil16291@gmail.com>
2018-10-09 22:36:18 +0530

    latest changes
    latest changes

--- ModernMarketResearch/Controllers/InquiryFormController.cs
+++ ModernMarketResearch/Controllers/InquiryFormController.cs
@@ -67,11 +67,11 @@ namespace ExcellentMarketResearch.Controllers
             }
             if (FormType == "SampleRequestForm")
             {
-                ViewBag.FormTitle = "<b>Please fill your details below, to receive sample report:</b><div style='font-size:15px;width:100%;'><label style='color:red; display:inline;'>*</label> Indicates required fields</div>";
+                ViewBag.FormTitle = "<b>Please submit the form will contact you within 24 hours. :</b><div style='font-size:15px;width:100%;'><label style='color:red; display:inline;'>*</label> Indicates required fields</div>";
             }
             else
             {
-                ViewBag.FormTitle = "<b>Please submit the below form, to get more about this report:</b><div style='font-size:15px;width:100%;'><label style='color:red; display:inline;'>*</label> Indicates required fields</div>";
+                ViewBag.FormTitle = "<b>Please submit the form will contact you within 24 hours. :</b><div style='font-size:15px;width:100%;'><label style='color:red; display:inline;'>*</label> Indicates required fields</div>";
             }
 
             Session["Captcha"] = DrawCaptcha();
@@ -84,7 +84,7 @@ namespace ExcellentMarketResearch.Controllers
             return PartialView(Enquiredata);
 
         }
-    
+
         public ActionResult SampleRequest(int? ReportID)
         {
             InquiryVM Enquiredata = new InquiryVM();
@@ -117,7 +117,7 @@ namespace ExcellentMarketResearch.Controllers
             //{
             //    ViewBag.FormTitle = "<b>Please submit the below form, to get more about this report:</b><div style='font-size:15px;width:100%;'><label style='color:red; display:inline;'>*</label> Indicates required fields</div>";
             //}
-
+            ViewBag.FormTitle = "<b>Please submit the form will contact you within 24 hours. :</b><div style='font-size:15px;width:100%;'><label style='color:red; display:inline;'>*</label> Indicates required fields</div>";
             Session["Captcha"] = DrawCaptcha();
             var PlainText = Session["Captcha"].ToString();
             var EncryCaptcha = ExcellentMarketResearch.Areas.Admin.Models.Common.Encrypt(PlainText);
@@ -162,37 +162,38 @@ namespace ExcellentMarketResearch.Controllers
 
             Emailsending objEmailsending = new Emailsending();
             var response = Request["g-recaptcha-response"];
-           // string secreatekey = "6LcatdW0UAAAAAERSrddZFxQdvJd0xum_wTLvhUIT";
+            string secreatekey = "6LcatdW0UAAAAAERSrddZFxQdvJd0xum_wTLvhUIT";
             var client = new WebClient();
-            //var result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret=(0)&response=(1)", secreatekey, response));
-            //var obj = JObject.Parse(result);
-            //var status = (bool)obj.SelectToken("success");
-            //ViewBag.Message = status ? "Google recaptcha validation success" : "Google recaptcha validation fails";
+            var result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret=(0)&response=(1)", secreatekey, response));
+            var obj = JObject.Parse(result);
+            var status = (bool)obj.SelectToken("success");
+            // ViewBag.Message = status ? "Google recaptcha validation success" : "Google recaptcha validation fails";
             CustomerInquiry e = new CustomerInquiry();
             var Formname = string.Empty;
             int FormTypeId;
             string Publisher = string.Empty;
-
-            if (ModelState.IsValid )
+            if (status == true)
             {
-                // cap = Session["Captcha"].ToString();
-                cap = ExcellentMarketResearch.Areas.Admin.Models.Common.Decrypt(eq.RealCaptcha);
-                
-                if (eq.ReportId > 0)
+                if (ModelState.IsValid)
                 {
-                    var Publish = (from l in db.ReportMasters
-                                   join p in db.PublisherMasters on l.PublishereId equals p.PublisherId
-                                   where l.ReportId == eq.ReportId
-                                   select p).FirstOrDefault();
-                    Publisher = Publish.PublisherName;
-                }
-                else
-                {
-                    Publisher = "!";
-                }
+                    // cap = Session["Captcha"].ToString();
+                    cap = ExcellentMarketResearch.Areas.Admin.Models.Common.Decrypt(eq.RealCaptcha);
 
-                //if (eq.CaptchaCode == cap)
-               //{
+                    if (eq.ReportId > 0)
+                    {
+                        var Publish = (from l in db.ReportMasters
+                                       join p in db.PublisherMasters on l.PublishereId equals p.PublisherId
+                                       where l.ReportId == eq.ReportId
+                                       select p).FirstOrDefault();
+                        Publisher = Publish.PublisherName;
+                    }
+                    else
+                    {
+                        Publisher = "!";
+                    }
+
+                    //if (eq.CaptchaCode == cap)
+                    //{
                     CustomerInquiry cst = new CustomerInquiry();
                     var IpAddress = ExcellentMarketResearch.Models.PaymentGateway.IPAddress.GetIPAddress();
                     cst.Company = eq.Company;
@@ -234,16 +235,16 @@ namespace ExcellentMarketResearch.Controllers
                         //db.Entry(cst).State = EntityState.Added;
                         db.SaveChanges();
 
-                       // QYGroupRepository.PaymentGateway.Emailsending objEmailsending = new QYGroupRepository.PaymentGateway.Emailsending();
-                       // string ipAddress = QYGroupRepository.PaymentGateway.IPAddress.GetIPAddress();
-                       // Task.Run(() => new CRMWebService.WebServiceSoapClient().InsertUpdateKey(0, eq.ReportId, eq.ReportTitle, FormTypeId, 34, 1, 1, eq.ReportUrl, ipAddress, eq.Name, eq.EmailId, eq.AreaCode, eq.Company, eq.Designation, "!", "!", eq.Country, "!", eq.CustomerMessage, 1, "!", "!", "!", Publisher, 38, "BW&Zk^HfZ44P339nEzqrrawY4HL_VXw-5f+%8b4Hdw?$?m$G*!+kCGLK%3JjDn-74NY*LyhdJr6RAte&8MBWy6F2j82+qn7ap&DB@z-*q3sdH*#D-kwACucyaM7vzet4pSa?m^xnP@3zN5K9=*L6WLpDurTSuVTR3Hd&3XLHJnCcR!h*dL#fQhp^*#25LEFrMTt@z&8RWdf^CQcj!QrQU^WkdC5$Ub$8qnu!g7?*$$4%%M9?8spAugyCzZg5@dLGBNS_^7?x3VczR75J&=+9yFDVg*Qpd@R^_Jz-GtWgHxv4Kf$=2pxT@bqhx%aqgzZAN6RzZZ%rNX7km3fu$h?Z=+V3b_MQPLAxJBVT!=Ta+7Xd?CF3#4w44L@HU%nf4m#y-d2vgn6Gp2t7w!qFY%kN#y6DNAy#TbrZnqnjMtgeAd%BHSm9H29z4G_?qnBHE5J2EyutZ2RSh?P2fUE-sF8bNFdre@G^qQ??JzJuDCT3hby2py#+yfg*jC%&YBkrutHs"));
+                        // QYGroupRepository.PaymentGateway.Emailsending objEmailsending = new QYGroupRepository.PaymentGateway.Emailsending();
+                        // string ipAddress = QYGroupRepository.PaymentGateway.IPAddress.GetIPAddress();
+                        // Task.Run(() => new CRMWebService.WebServiceSoapClient().InsertUpdateKey(0, eq.ReportId, eq.ReportTitle, FormTypeId, 34, 1, 1, eq.ReportUrl, ipAddress, eq.Name, eq.EmailId, eq.AreaCode, eq.Company, eq.Designation, "!", "!", eq.Country, "!", eq.CustomerMessage, 1, "!", "!", "!", Publisher, 38, "BW&Zk^HfZ44P339nEzqrrawY4HL_VXw-5f+%8b4Hdw?$?m$G*!+kCGLK%3JjDn-74NY*LyhdJr6RAte&8MBWy6F2j82+qn7ap&DB@z-*q3sdH*#D-kwACucyaM7vzet4pSa?m^xnP@3zN5K9=*L6WLpDurTSuVTR3Hd&3XLHJnCcR!h*dL#fQhp^*#25LEFrMTt@z&8RWdf^CQcj!QrQU^WkdC5$Ub$8qnu!g7?*$$4%%M9?8spAugyCzZg5@dLGBNS_^7?x3VczR75J&=+9yFDVg*Qpd@R^_Jz-GtWgHxv4Kf$=2pxT@bqhx%aqgzZAN6RzZZ%rNX7km3fu$h?Z=+V3b_MQPLAxJBVT!=Ta+7Xd?CF3#4w44L@HU%nf4m#y-d2vgn6Gp2t7w!qFY%kN#y6DNAy#TbrZnqnjMtgeAd%BHSm9H29z4G_?qnBHE5J2EyutZ2RSh?P2fUE-sF8bNFdre@G^qQ??JzJuDCT3hby2py#+yfg*jC%&YBkrutHs"));
 
                         //Auto Mailer
-                        //Task.Run(() => objEmailsending.SendEmail("sales@marketresearchtrade.com", "Sales", cst.EmailId, "", "balasaheb.p@marketresearchstore.com", "MarketResearchTrade.com : " + eq.ReportTitle, GenerateMailBody_RequestSample_AutoReply(cst.Name, eq.ReportTitle)));
+                        Task.Run(() => objEmailsending.SendEmail("sales@excellentmarketresearch.com", "Sales", cst.EmailId, "", "balasahebpatil1612@gmail.com", "Excellentmarketresearch.com : " + eq.ReportTitle, objEmailsending.GenerateMailBody_RequestSample_AutoReply(cst.Name, eq.ReportTitle)));
 
                         //To company
-                       // Task.Run(() => objEmailsending.SendEmail("sales@excellentmarketresearcg.com", "Sales", "balasahebpatil1612@gmail.com", "", "", "ExcellentMarketResearch.com" + " : " + Formname,objEmailsending.GenerateMailBody_RequestSample(eq.ReportTitle, cst.Name, cst.EmailId, cst.PhoneNumber, cst.Company, cst.Country, cst.Designation, cst.CustomerMessage)));
-                        Task.Run(() => objEmailsending.SendEmail("balasahebpatil1612@gmail.com", "Sales", "balasahebpatil1612@gmail.com", "", "", "ExcellentMarketResearch.com" + " : " + Formname, GenerateMailBody_RequestSample(eq.ReportTitle, cst.Name, cst.EmailId, cst.PhoneNumber, cst.Company, cst.Country, cst.Designation, cst.CustomerMessage, IpAddress)));
+                        // Task.Run(() => objEmailsending.SendEmail("sales@excellentmarketresearcg.com", "Sales", "balasahebpatil1612@gmail.com", "", "", "ExcellentMarketResearch.com" + " : " + Formname,objEmailsending.GenerateMailBody_RequestSample(eq.ReportTitle, cst.Name, cst.EmailId, cst.PhoneNumber, cst.Company, cst.Country, cst.Designation, cst.CustomerMessage)));
+                        Task.Run(() => objEmailsending.SendEmail("sales@excellentmarketresearch.com", "Sales", "balasahebpatil1612@gmail.com", "", "", "ExcellentMarketResearch.com" + " : " + Formname, objEmailsending.GenerateMailBody_RequestSample(eq.ReportTitle, cst.Name, cst.EmailId, cst.PhoneNumber, cst.Company, cst.Country, cst.Designation, cst.CustomerMessage, IpAddress)));
 
                         Session["Name"] = cst.Name;
 
@@ -260,11 +261,12 @@ namespace ExcellentMarketResearch.Controllers
                             }
                         }
                     }
-                //}
-                //Return the if model not valid
+                    //}
+                    //Return the if model not valid
+                    return View(eq);
+                }
                 return View(eq);
             }
-
             return View(eq);
         }
 
@@ -315,24 +317,24 @@ namespace ExcellentMarketResearch.Controllers
             return File(ms.ToArray(), "image/png");
 
         }
-                       
-        private string GenerateMailBody_RequestSample(string reporttitle, string name, string emailid, string contactno, string nameofcompany, string countryname, string designation, string customermessage, string ipaddress)
-        {
-            string result = "";
-            result = "dear admin,<br /><br />" + "<table>";
-            result += reporttitle != "" ? "<tr> <td valign='top' width='30%'><b>report title</b></td>   <td valign='top' width='2%'><b> : </b></td> <td valign='top' width='68%'>" + reporttitle + "</td> </tr>" : "";
-            result += name != "" ? "<tr> <td valign='top'><b>customer name</b></td>  <td valign='top'><b> : </b></td> <td valign='top'>" + name + "</td> </tr>" : "";
-            result += emailid != "" ? "<tr> <td valign='top'><b>email id</b></td>       <td valign='top'><b> : </b></td> <td valign='top'>" + emailid + "</td> </tr>" : "";
-            result += contactno != "" ? "<tr> <td valign='top'><b>phone</b></td>          <td valign='top'><b> : </b></td> <td valign='top'>" + contactno + "</td> </tr>" : "";
-            result += nameofcompany != "" ? "<tr> <td valign='top'><b>company name</b></td>   <td valign='top'><b> : </b></td> <td valign='top'>" + nameofcompany + "</td> </tr>" : "";
-            result += designation != "" ? "<tr> <td valign='top'><b>designation</b></td>    <td valign='top'><b> : </b></td> <td valign='top'>" + designation + "</td> </tr>" : "";
-            result += countryname != "" ? "<tr> <td valign='top'><b>country name</b></td>   <td valign='top'><b> : </b></td> <td valign='top'>" + countryname + "</td> </tr>" : "";
-            result += customermessage != "" ? "<tr> <td valign='top'><b>enquiry text</b></td>   <td valign='top'><b> : </b></td> <td valign='top'>" + customermessage + "</td> </tr>" : "";
-            result += "<tr> <td valign='top'><b>publisher</b></td>      <td valign='top'><b> : </b></td> <td valign='top'>" + "Excellent Market Research " + "</td> </tr>";
-            result += "<tr> <td valign='top'><b>ip address</b></td>     <td valign='top'><b> : </b></td> <td valign='top'>" + ipaddress == null ? ExcellentMarketResearch.Models.PaymentGateway.IPAddress.GetIPAddress() : ipaddress + "</td> </tr>";
-            result += "</table>";
-            return result;
-        }
+
+        //public string GenerateMailBody_RequestSample(string reporttitle, string name, string emailid, string contactno, string nameofcompany, string countryname, string designation, string customermessage, string ipaddress)
+        //{
+        //    string result = "";
+        //    result = "dear admin,<br /><br />" + "<table>";
+        //    result += reporttitle != "" ? "<tr> <td valign='top' width='30%'><b>report title</b></td>   <td valign='top' width='2%'><b> : </b></td> <td valign='top' width='68%'>" + reporttitle + "</td> </tr>" : "";
+        //    result += name != "" ? "<tr> <td valign='top'><b>customer name</b></td>  <td valign='top'><b> : </b></td> <td valign='top'>" + name + "</td> </tr>" : "";
+        //    result += emailid != "" ? "<tr> <td valign='top'><b>email id</b></td>       <td valign='top'><b> : </b></td> <td valign='top'>" + emailid + "</td> </tr>" : "";
+        //    result += contactno != "" ? "<tr> <td valign='top'><b>phone</b></td>          <td valign='top'><b> : </b></td> <td valign='top'>" + contactno + "</td> </tr>" : "";
+        //    result += nameofcompany != "" ? "<tr> <td valign='top'><b>company name</b></td>   <td valign='top'><b> : </b></td> <td valign='top'>" + nameofcompany + "</td> </tr>" : "";
+        //    result += designation != "" ? "<tr> <td valign='top'><b>designation</b></td>    <td valign='top'><b> : </b></td> <td valign='top'>" + designation + "</td> </tr>" : "";
+        //    result += countryname != "" ? "<tr> <td valign='top'><b>country name</b></td>   <td valign='top'><b> : </b></td> <td valign='top'>" + countryname + "</td> </tr>" : "";
+        //    result += customermessage != "" ? "<tr> <td valign='top'><b>enquiry text</b></td>   <td valign='top'><b> : </b></td> <td valign='top'>" + customermessage + "</td> </tr>" : "";
+        //    result += "<tr> <td valign='top'><b>publisher</b></td>      <td valign='top'><b> : </b></td> <td valign='top'>" + "Excellent Market Research " + "</td> </tr>";
+        //    result += "<tr> <td valign='top'><b>ip address</b></td>     <td valign='top'><b> : </b></td> <td valign='top'>" + ipaddress == null ? ExcellentMarketResearch.Models.PaymentGateway.IPAddress.GetIPAddress() : ipaddress + "</td> </tr>";
+        //    result += "</table>";
+        //    return result;
+        //}
 
 
         //public string GenerateMailBody_RequestSample_AutoReply(string Name, string ReportTitle)
@@ -347,10 +349,10 @@ namespace ExcellentMarketResearch.Controllers
         //            //+ "<br /><br />I'll contact you soon to serve your research needs."
         //             + "<br /><br />We'll contact you soon to serve your research needs."
         //             + "<b><br /><br />Warm regards,"
-        //             + "<br />Joel John | Corporate Sales Specialist,USA"
-        //             + "<br />Direct line: + 1-855-465-4651"
-        //             + "<br />" + "marketresearchtrade.com"
-        //             + "<br />E-mail: joel@qygroup.biz | Web: " + "ExcellentMarketResearch.com" + "</b>";
+        //             + "<br />Miler Jhon | Corporate Sales Specialist,USA"
+        //             + "<br />Direct line: + 1888-868686-68686#"
+        //             + "<br />" + "excellentmarketresearch.com"
+        //             + "<br />E-mail: sales@excellentmarketresearch.com | Web: " + "excellentmarketresearch.com" + "</b>";
         //    }
         //    else
         //    {
@@ -361,10 +363,10 @@ namespace ExcellentMarketResearch.Controllers
         //            //+ "<br /><br />" + "QYGroup.biz".Substring(0, "QYGroup.biz".Length - 1) + ReportURL
         //            + "<br /><br />We'll contact you soon to serve your research needs."
         //            + "<b><br /><br />Warm regards,"
-        //            + "<br />Joel John | Corporate Sales Specialist,USA"
-        //            + "<br />Direct line: + 1-855-465-4651"
-        //            + "<br />" + "marketresearchtrade.com"
-        //            + "<br />E-mail: joel@qygroup.biz | Web: " + "marketresearchtrade.com" + "</b>";
+        //            + "<br />Miler Jhon | Corporate Sales Specialist,USA"
+        //            + "<br />Direct line: +  1888-868686-68686#"
+        //            + "<br />" + "excellentmarketresearch.com"
+        //            + "<br />E-mail: sales@excellentmarketresearch.com  | Web: " + "excellentmarketresearch.com" + "</b>";
         //    }
         //    return result;
         //}
